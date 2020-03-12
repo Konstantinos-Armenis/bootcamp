361bb238624bc116b87c04106a1169fa96cc443b
Konstantinos84 <62116471+Konstantinos84@users.noreply.github.com>
2020-03-12 21:34:03 +0200

    Add files via upload

---
+++ ASKHSH 2 (2)/ASKHSH 2 (2)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASKHSH_2__2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Give me a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num%2 == 0)
+            {
+                Console.WriteLine(num + 2);
+            }
+            else
+            {
+                Console.WriteLine(" Einai arritos");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASKHSH_ME_TERNARY_OPERATOR_TETRADIO_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+            int b = 31;
+            
+           var max = (a > b) ? a : b;
+            Console.WriteLine(max);
+        }
+    }
+}
